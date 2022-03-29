e9186e9ece79e8ce88477199d3ff9941787db881
krakow10 <krakow20@gmail.com>
2022-03-28 21:49:51 -0700

    fix multi_den = 1 case

--- vsrife/__init__.py
+++ vsrife/__init__.py
@@ -96,7 +96,11 @@ def RIFE(clip: vs.VideoNode, multi: int = 2, multi_den: int = 1, scale: float = 
     clip0 = vs.core.std.Interleave([clip] * multi)
     clip1 = clip.std.DuplicateFrames(frames=clip.num_frames - 1).std.DeleteFrames(frames=0)
     clip1 = vs.core.std.Interleave([clip1] * multi)
-    return clip0.std.ModifyFrame(clips=[clip0, clip1], selector=rife).std.SelectEvery(cycle = multi_den, offsets = 0)
+    clip2 = clip0.std.ModifyFrame(clips=[clip0, clip1], selector=rife)
+    if multi_den == 1:
+        return clip2
+    else:
+        return clip2.std.SelectEvery(cycle = multi_den, offsets = 0)
 
 
 def frame_to_tensor(f: vs.VideoFrame) -> torch.Tensor:
