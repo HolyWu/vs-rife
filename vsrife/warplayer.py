6bba2afc8375b82e1b56f4a9987d239962158651
HolyWu <holywu@gmail.com>
2022-10-22 09:57:10 +0800

    Fix corruption with FP16 mode on 4K video

--- vsrife/warplayer.py
+++ vsrife/warplayer.py
@@ -4,11 +4,14 @@ backwarp_tenGrid = {}
 
 
 def warp(tenInput, tenFlow, device):
+    orig_dtype = tenInput.dtype
+    tenInput = tenInput.float()
+    tenFlow = tenFlow.float()
     k = (str(tenFlow.device), str(tenFlow.size()))
     if k not in backwarp_tenGrid:
-        tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3], device=device).view(
+        tenHorizontal = torch.linspace(-1.0, 1.0, tenFlow.shape[3], dtype=torch.float, device=device).view(
             1, 1, 1, tenFlow.shape[3]).expand(tenFlow.shape[0], -1, tenFlow.shape[2], -1)
-        tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2], device=device).view(
+        tenVertical = torch.linspace(-1.0, 1.0, tenFlow.shape[2], dtype=torch.float, device=device).view(
             1, 1, tenFlow.shape[2], 1).expand(tenFlow.shape[0], -1, -1, tenFlow.shape[3])
         backwarp_tenGrid[k] = torch.cat(
             [tenHorizontal, tenVertical], 1)
@@ -17,4 +20,7 @@ def warp(tenInput, tenFlow, device):
                          tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0)], 1)
 
     g = (backwarp_tenGrid[k] + tenFlow).permute(0, 2, 3, 1)
-    return torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
+    output = torch.nn.functional.grid_sample(input=tenInput, grid=g, mode='bilinear', padding_mode='border', align_corners=True)
+    if orig_dtype == torch.half:
+        output = output.half()
+    return output
